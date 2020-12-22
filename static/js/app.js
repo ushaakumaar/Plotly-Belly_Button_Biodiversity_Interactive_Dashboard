cb516d661c0832fd13fa4c0e00a3499968029e34
Usha Saravanakumar <67562186+ushaakumaar@users.noreply.github.com>
2020-12-22 00:16:11 -0500

    Wrote function to render the bar chart for the user selected ID

--- static/js/app.js
+++ static/js/app.js
@@ -40,3 +40,49 @@ function loadMetaData(value, filteredMetaData) {
         var valueCell = row.append("td").text(`${value}`).style("padding", "1");
     });
 }
+
+// Function to render the bar chart for the user selected ID
+function loadHBar(value, filteredSampleData) {
+
+    // Slice the first 10 objects for plotting
+    slicedSampleValues = filteredSampleData.sample_values.slice(0, 10);
+    slicedOtuIds = filteredSampleData.otu_ids.slice(0, 10);
+    slicedOtuLabels = filteredSampleData.otu_labels.slice(0, 10);
+
+    // Reverse the array to accommodate Plotly's defaults
+    reversedSampleValues = slicedSampleValues.reverse();
+    reversedOtuIds = slicedOtuIds.reverse();
+    reversedOtuIds = reversedOtuIds.map(id => `OTU ${id}`);
+    reversedOtuLabels = slicedOtuLabels.reverse();
+
+    // Trace for the OTU Data
+    var traceHBar = {
+        x: reversedSampleValues,
+        y: reversedOtuIds,
+        text: reversedOtuLabels,
+        type: 'bar',
+        orientation: 'h'
+    };
+
+    // Data
+    var dataHBar = [traceHBar];
+
+    // Layout for creating bar chart
+    var layoutHBar = {
+        showlegend: false,
+        margin: {
+            t: 30,
+            b: 40
+        },
+        xaxis: {
+            title: 'Number of Samples',
+        },
+        yaxis: {
+            title: 'OTU IDs',
+        },
+        title: `Top 10 OTUs for Test Subject ID - ${value}`,
+    };
+
+    // Render the bar plot to the div tag with id "bar"
+    Plotly.newPlot('bar', dataHBar, layoutHBar);
+}
