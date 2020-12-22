19d5e5e3c75045fe5d3cb53d1f5d425d6b61e4b7
Usha Saravanakumar <67562186+ushaakumaar@users.noreply.github.com>
2020-12-22 00:10:55 -0500

    Wrote function to populate the ID Dropdown html element

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,22 @@
 // variables to hold data from JSON file
 var ids = [];
 var metadatas = [];
-var samples = [];										
+var samples = [];
+
+/***************************************************
+USER DEFINED FUNCTIONS
+****************************************************/
+
+// Function to populate the ID Dropdown html element
+function populateIDDropdown() {
+
+    // Retrieve the ID Dropdown html element
+    var selDataset = d3.select('#selDataset');
+
+    // Populate the dropdown element with IDs
+    ids.forEach(id => {
+        var cell = selDataset.append('option');
+        cell.property('value', id).text(id);
+    });
+
+}
