bef616d270c3107e7718d48aea1b29441def11f6
Usha Saravanakumar <67562186+ushaakumaar@users.noreply.github.com>
2020-12-22 00:12:47 -0500

    Wrote function to render the Demographic Info for the user selected ID

--- static/js/app.js
+++ static/js/app.js
@@ -20,3 +20,23 @@ function populateIDDropdown() {
     });
 
 }
+
+// Function to render the Demographic Info for the user selected ID
+function loadMetaData(value, filteredMetaData) {
+
+    // Retrieve the Div tag to display the demographic info 
+    var metadata = d3.select('#sample-metadata');
+
+    // Clear the contents of the Div Tag
+    metadata.html('');
+
+    // Render the Demographic Info
+    // var table = metadata.append("table").classed("table", true);
+    var table = metadata.append("table");
+    Object.entries(filteredMetaData).forEach(([key, value]) => {
+        var row = table.append("tr");
+        var initCapKey = key[0].toUpperCase() + key.substr(1).toLowerCase();
+        var keyCell = row.append("th").text(`${initCapKey}`);
+        var valueCell = row.append("td").text(`${value}`).style("padding", "1");
+    });
+}
